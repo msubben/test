d40e3eca5453ccc4f53af01c903c5c2297783b62
msubben <32372618+msubben@users.noreply.github.com>
2023-05-17 11:47:58 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     scene.setBackgroundColor(5)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    scene.setBackgroundColor(6)
+    scene.setBackgroundColor(2)
 })
 scene.setBackgroundColor(1)
