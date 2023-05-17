b5500eaa9d3dfe5e64c26f7a6b28178b1639871c
msubben <32372618+msubben@users.noreply.github.com>
2023-05-17 11:41:34 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    scene.setBackgroundColor(5)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    scene.setBackgroundColor(6)
+})
+scene.setBackgroundColor(1)
